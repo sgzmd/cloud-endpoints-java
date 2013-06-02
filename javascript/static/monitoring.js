9be302bf9eaec62afadcd4b4ab766787fb958865
Roman Kirillov <me@romankirillov.info>
2013-06-02 20:43:00 +0100

    Some progress with JS client

--- javascript/static/monitoring.js
+++ javascript/static/monitoring.js
@@ -1,3 +1,48 @@
+var handleCheckbox = function(cb, networkId) {
+    alert(networkId);
+}
+
+var disarmRoom = function(roomId) {
+
+}
+
+/**
+ * It's ugly as a sin, but it works. It will produce something like this:
+ *
+ * <div>
+ *  <input onclick="handleCheckbox(this, "DddEeeFff");"
+ *         type="checkbox"
+ *         id="sensor_0_DddEeeFff">
+ *      <label for="sensor_0_DddEeeFff">DddEeeFff</label>
+ * </div>
+ */
+var makeCheckBox = function(roomId, name, networkId, checked, orderNumber) {
+    return "<div class='cb'><input disabled onclick='handleCheckbox(this, \"" + networkId + "\");' type='checkbox'" +
+        "id='sensor_" + roomId + "_" + networkId + "' " + checked + "/>" +
+        "<label for='sensor_" + roomId + "_" + networkId + "'>" + name +
+        "</label></div>";
+
+}
+
+function makeRoomHtml(room, roomId) {
+    var html = "";
+    for (var item in room.sensors) {
+        var sensor = room.sensors[item];
+        var checked = sensor.active ? "checked" : ""
+
+        html += makeCheckBox(
+            roomId,
+            sensor.networkId,
+             sensor.networkId,
+             checked,
+             item);
+    }
+
+    html += "<div class='room-buttons'><button>Disarm room</button>&nbsp;<button>Arm room</button></div>";
+
+    return html;
+}
+
 function init() {
     var ROOT = 'https://cloud-endpoints-example.appspot.com/_ah/api';
     gapi.client.load('monitoring', 'v1', function () {
@@ -11,12 +56,12 @@ function init() {
                     var room = resp.items[item];
                     var num_tabs = $("div#tabs ul li").length + 1;
                     $("div#tabs").append(
-                        "<div id='tab" + num_tabs + "'>" + room.name + "</div>"
-                    );
+                        "<div id='tab" + num_tabs + "'>" + makeRoomHtml(room, item) + "</div>"
+                        );
 
                     $("div#tabs ul").append(
                         "<li><a href='#tab" + num_tabs + "'>" + room.name + "</a></li>"
-                    );
+                        );
 
                 }
                 $("div#tabs").tabs("refresh");
