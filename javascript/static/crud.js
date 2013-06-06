75efa527454dd0ccdbcba640be7d047d68dadb59
Roman Kirillov <me@romankirillov.info>
2013-06-06 22:28:34 +0300

    More crud

--- javascript/static/crud.js
+++ javascript/static/crud.js
@@ -6,64 +6,69 @@
  * To change this template use File | Settings | File Templates.
  */
 
-var name = $("#name");
-var allFields = [name];
-
-function updateTips( t ) {
-  tips
-      .text( t )
-      .addClass( "ui-state-highlight" );
-  setTimeout(function() {
-    tips.removeClass( "ui-state-highlight", 1500 );
-  }, 500 );
+function updateTips(t) {
+  var tips = $('#validateTips');
+  tips.text(t).addClass("ui-state-highlight");
+  setTimeout(function () {
+    tips.removeClass("ui-state-highlight", 1500);
+  }, 500);
 }
 
-function checkLength( o, n, min, max ) {
-  if ( o.val().length > max || o.val().length < min ) {
-    o.addClass( "ui-state-error" );
-    updateTips( "Length of " + n + " must be between " +
-        min + " and " + max + "." );
+function checkLength(o, n, min, max) {
+  if (o.val().length > max || o.val().length < min) {
+    o.addClass("ui-state-error");
+    updateTips("Length of " + n + " must be between " +
+        min + " and " + max + ".");
     return false;
   } else {
     return true;
   }
 }
 
+var addRoom = function(name) {
+  var room = {
+    'name': name
+  };
+
+  console.log("Will be adding a room");
+  console.log(room);
+
+  // gapi.client.monitoring.rooms.
+};
+
 var initUI = function () {
+  var name = $("#name");
+
   $("#create-room")
       .button()
       .click(function () {
         $("#dialog-form").dialog("open");
       });
 
-  $( "#dialog-form" ).dialog({
+  $("#dialog-form").dialog({
     autoOpen: false,
     height: 300,
     width: 350,
     modal: true,
     buttons: {
-      "Create an account": function() {
+      "Add new room": function () {
+        var name = $('#name');
         var bValid = true;
-        name.removeClass( "ui-state-error" );
+        name.removeClass("ui-state-error");
 
-        bValid = bValid && checkLength( name, "username", 3, 16 );
+        bValid = bValid && checkLength(name, "username", 3, 16);
 
-        if ( bValid ) {
-          alert("Everything is OK, will be adding a room with name " + name.val());
-//          $( "#users tbody" ).append( "<tr>" +
-//              "<td>" + name.val() + "</td>" +
-//              "<td>" + email.val() + "</td>" +
-//              "<td>" + password.val() + "</td>" +
-//              "</tr>" );
-          $( this ).dialog( "close" );
+        if (bValid) {
+          addRoom(name.val());
+          $(this).dialog("close");
         }
       },
-      Cancel: function() {
-        $( this ).dialog( "close" );
+      'Cancel': function () {
+        $(this).dialog("close");
       }
     },
-    close: function() {
-      allFields.val( "" ).removeClass( "ui-state-error" );
+    close: function () {
+      name.val("").removeClass("ui-state-error");
     }
   });
 };
