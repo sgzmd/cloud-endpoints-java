7bc1a201f323d4d93fef7acfe6f1471f408c418b
sgzmd <sigizmund@gmail.com>
2013-06-16 15:47:50 +0100

    Removing old monitoring client which didn't work anyway

--- android/MonitoringClient/src/com/sgzmd/examples/monitoringclient/android/MonitoringDataProvider.java
+++
@@ -1,9 +0,0 @@
-package com.sgzmd.examples.monitoringclient.android;
-
-import java.util.List;
-
-import com.google.api.services.monitoring.model.Room;
-
-public interface MonitoringDataProvider {
-  List<Room> getRooms();
-}
